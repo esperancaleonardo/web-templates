a6d4f68fb91a4490f5da845b4586fd12c61b20b8
Leonardo de Oliveira Esperanca <leonardo.esperanca@azi.com.br>
2023-04-22 15:07:01 -0300

    configurando vuetify e pinia no projeto vue

--- vite-vue-app/src/main.js
+++ vite-vue-app/src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import { createPinia } from "pinia";
+import { loadFonts } from "./plugins/webfontloader";
 
-createApp(App).mount("#app");
+loadFonts();
+
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.use(vuetify);
+app.mount("#app");
